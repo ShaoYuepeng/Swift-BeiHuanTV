52fb95d2e9577322072b6933cf25fc2143063523
邵 <shao@s-macbook-pro.local>
2016-01-09 12:03:43 +0800

    初始化项目

---
+++ BHTV/BHTV/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  BHTV
+//
+//  Created by 丫了个P on 16/1/9.
+//  Copyright © 2016年 邵岳鹏. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        return true
+    }
+
+   }
+
---
+++ BHTV/BHTV/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  BHTV
+//
+//  Created by 丫了个P on 16/1/9.
+//  Copyright © 2016年 邵岳鹏. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+
+
+}
+
